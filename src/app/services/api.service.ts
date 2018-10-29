2152071b0bcf1a547ae973b36f1efb06cc1c2f00
harsha kavinda <hpkavind@gmail.com>
2018-10-29 13:21:45 +0530

    new comments added

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,19 +7,20 @@ export class RestService {
 
   constructor(private http: HttpClient) { }
 
+  // protected get request
   protected get(url: string, headers: any): Observable<any> {
     console.log("get req sent");
     return this.http.get(url, {headers: headers});
   }
-
+  // protected Post request
   protected post(url: string, data: any, headers: any): Observable<any> {
     return this.http.post(url, data, {headers: headers});
   }
-
+  // protected put request
   protected put(url: string, data: any, headers: any): Observable<any> {
     return this.http.put(url, data, {headers: headers});
   }
-
+  // protected delete request
   protected delete(url: string, headers: any): Observable<any> {
     return this.http.delete(url, {headers: headers});
   }
